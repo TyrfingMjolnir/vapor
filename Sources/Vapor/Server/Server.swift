a920195f7074a64b2c16abd7c19699d2757d9f3d
Tyrfing Mjølner <4lphamal3@gmail.com>
2019-11-07 10:47:49 +0100

    Update Server.swift

--- Sources/Vapor/Server/Server.swift
+++ Sources/Vapor/Server/Server.swift
@@ -1,5 +1,6 @@
 public protocol Server {
     func start(hostname: String?, port: Int?) throws
+    func start(socket: String?) throws
     var onShutdown: EventLoopFuture<Void> { get }
     func shutdown()
 }
